d82e7b186ad677e45ff7012cc628d8852574c20a
kamran <kamran@vaultspay.ae>
2024-02-21 17:04:17 +0500

    user login test changes

--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -5,3 +5,9 @@ test('user entered invalid fields', function () {
     $response->assertStatus(422);
     $response->assertJsonValidationErrors(['email', 'password']);
 });
+
+test('user can login', function () {
+    $response = $this->postJson('/api/login');
+    $response->assertStatus(200);
+//    $response->assertJsonValidationErrors(['email', 'password']);
+});
