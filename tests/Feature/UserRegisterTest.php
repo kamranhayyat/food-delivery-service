b3e4e27121cf9f5c2c434888bda8f952ee771520
kamran <kamranhayyat93@gmail.com>
2024-02-26 23:39:11 +0500

    unit and feature tests

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 it('user tries to register with empty request', function () {
     $response = $this->postJson('/api/register');
@@ -13,9 +14,20 @@ it('user tries to register with invalid request', function () {
         'name' => 'user',
         'email' => 'user@test.com',
         'password' => 'admin123@',
+        'password_confirmation' => 'admin123',
+        'phone' => '0313502814'
+    ]);
+    $response->assertStatus(422);
+});
+
+it('user tries to register with valid request', function () {
+    $response = $this->postJson('/api/register', [
+        'name' => 'user',
+        'email' => 'user@test.com',
+        'password' => 'admin123@',
         'password_confirmation' => 'admin123@',
         'phone' => '03135028148'
     ]);
     $response->assertOk();
-    $response->dump();
+    $response->assertJson(fn(AssertableJson $json) => $json->has('data.user')->where('message', 'User registered successfully!'));
 });
